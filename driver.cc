ab8dfcceb9f61de201dbb3480709ae36da90c434
Patrick Kyoyetera <pertriquek171@gmail.com>
2016-10-18 23:32:47 -0400

    Update driver.cc

--- driver.cc
+++ driver.cc
@@ -20,21 +20,5 @@ int main(int argc, char* argv[]) {
 		cout<<"Continue?"<<endl;
 		cin>>ans;
 	}while(ans=="yes" or ans=="y" or ans=="Y" or ans=="Yes" or ans=="YES");
-//	player(p2_hand)
-	
-	
-	
-	/*
-	for(vCard::iterator it=p1_hand.begin(); it!=p1_hand.end(); ++it)
-		cout << (*it).toString() << "\t"  << endl;
-		cout << "Hand 1 strength: " << evaluateHand(p1_hand) << endl;
-	cout << "\n\n";
-	for(vCard::iterator it=p2_hand.begin(); it!=p2_hand.end(); ++it)
-		cout << (*it).toString() << "\t"  << endl;
-		cout << "Hand 2 strength: " << evaluateHand(p2_hand) << endl;
-	cout << "\n\n";
-	betterHand(p1_hand, p2_hand);
-
-	*/
 
 }
